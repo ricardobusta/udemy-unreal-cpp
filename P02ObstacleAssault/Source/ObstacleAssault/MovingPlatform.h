46b782eabe64838ed7c64b53c8706c81f202bd0b
Ricardo Bustamante <ricardo@busta.dev>
2025-01-31 22:52:31 +0100

    Add base source for platforms

---
+++ P02ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 7;
+
+	UPROPERTY(EditAnywhere)
+	float MyFloat = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+	bool MyBool = false;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
